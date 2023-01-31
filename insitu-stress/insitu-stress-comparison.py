2d8fa9ad385b56a7bb1b0d5c18cd424f7e240cf4
daimessdn@gmail.com <daimessdn@gmail.com>
2023-01-31 13:59:54 +0700

    added comparison of both condifion in effective and absolute stress resultant

--- insitu-stress/insitu-stress-comparison.py
+++ insitu-stress/insitu-stress-comparison.py
@@ -6,6 +6,45 @@ fig.set_size_inches(7.5, 7.5)
 fig.suptitle("In-situ stress on Example 2.1 (Question 3)\nWith seawater specific gravity.")
 ax = fig.add_subplot(projection='3d')
 
+# STRIKE-SLIP STRESS STATE
+# horizontal stress
+seawater_sg = 1.03
+
+sigma_v = 1.81 * -1 # acuan stress vertical ke bawah
+                    ## karena batuan mengalami kompaksi
+
+sigma_h_min = 1.64
+sigma_h_max = 1.92
+
+# add scatter (dot) plot into every single sigma
+## to get the geological stress state
+ax.quiver(0, sigma_h_max, sigma_v, sigma_h_min, 0, 0, color=mcolors.CSS4_COLORS["maroon"], linewidths=2, linestyle="dashed")
+ax.quiver(sigma_h_min, 0, sigma_v, 0, sigma_h_max, 0, color=mcolors.CSS4_COLORS["maroon"], linewidths=2, linestyle="dashed")
+ax.quiver(sigma_h_min, sigma_h_max, 0, 0, 0, sigma_v, color=mcolors.CSS4_COLORS["maroon"], linewidths=2, linestyle="dashed")
+
+ax.scatter(sigma_h_min, sigma_h_max, sigma_v, label="Strike-slip Fault", color=mcolors.CSS4_COLORS["dodgerblue"])
+ax.quiver(0, 0, 0, sigma_h_min, sigma_h_max, sigma_v, color=mcolors.CSS4_COLORS["dodgerblue"], linewidths=2)
+
+# NORMAL STRESS STATE
+# seawater specific gravity
+seawater_sg = 1.03
+
+# vertical stress
+sigma_v = 1.92 * -1 # acuan stress vertical ke bawah
+                                    ## karena batuan mengalami kompaksi
+
+sigma_h_min = 1.75
+sigma_h_max = 1.77
+
+# add scatter (dot) plot into every single sigma
+## to get the geological stress state
+ax.quiver(0, sigma_h_max, sigma_v, sigma_h_min, 0, 0, color=mcolors.CSS4_COLORS["maroon"], linewidths=2, linestyle="dashed")
+ax.quiver(sigma_h_min, 0, sigma_v, 0, sigma_h_max, 0, color=mcolors.CSS4_COLORS["maroon"], linewidths=2, linestyle="dashed")
+ax.quiver(sigma_h_min, sigma_h_max, 0, 0, 0, sigma_v, color=mcolors.CSS4_COLORS["maroon"], linewidths=2, linestyle="dashed")
+
+ax.scatter(sigma_h_min, sigma_h_max, sigma_v, label="Normal Fault", color=mcolors.CSS4_COLORS["green"])
+ax.quiver(0, 0, 0, sigma_h_min, sigma_h_max, sigma_v, color=mcolors.CSS4_COLORS["green"], linewidths=2)
+
 # EFFECTIVE STRIKE-SLIP STRESS STATE
 # horizontal stress
 seawater_sg = 1.03
